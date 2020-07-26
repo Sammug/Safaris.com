96207c24e457e6ef231be6c99812a0ed9fc1ab54
Muigai David <sammug95@yahoo.com>
2020-07-26 22:57:07 +0300

    added some fragments

---
+++ app/src/main/java/com/example/etour/MpesaFrag.java
@@ -0,0 +1,28 @@
+package com.example.etour;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MpesaFrag extends Fragment {
+
+    public MpesaFrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mpesa, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/etour/PayWithVisaFrag.java
@@ -0,0 +1,30 @@
+package com.example.etour;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PayWithVisaFrag extends Fragment {
+
+    public PayWithVisaFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pay_with_visa, container, false);
+    }
+}
